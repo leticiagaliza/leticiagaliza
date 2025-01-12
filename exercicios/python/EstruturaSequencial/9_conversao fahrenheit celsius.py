f08f6f201e116552cca425a027c875685f3cf124
Letícia Monteiro <151076624+leticiamnteiro@users.noreply.github.com>
2025-01-12 13:37:58 -0300

    Add files via upload

---
+++ exercicios/python/EstruturaSequencial/9_conversao fahrenheit celsius.py
@@ -0,0 +1,5 @@
+temperatura_f = int(input('Digite o valor da temperatura em Fahrenheit: '))
+
+temperatura_celsius = 5 * ((temperatura_f - 32)/9)
+
+print(f'{temperatura_f}°F equivale a {temperatura_celsius}°C.')
