a9b6b255a3ce75033eacc75740f85535f9dd9be7
Letícia Monteiro <151076624+leticiamnteiro@users.noreply.github.com>
2025-01-12 13:21:23 -0300

    Add files via upload

---
+++ exercicios/python/EstruturaSequencial/7_area e dobro da area de um quadrado.py
@@ -0,0 +1,5 @@
+lado_quadrado = float(input('Digite o valor do lado do quadrado em centímetros: '))
+
+area_quadrado = lado_quadrado * lado_quadrado
+
+print(f'A área do quadrado resulta em {area_quadrado}. O dobro da área equivale a {area_quadrado*2}')
