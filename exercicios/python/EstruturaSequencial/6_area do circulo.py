4d149b38c444a7206e82303a983b6d23c3705e7c
Letícia Monteiro <151076624+leticiamnteiro@users.noreply.github.com>
2025-01-12 12:44:59 -0300

    Add files via upload

---
+++ exercicios/python/EstruturaSequencial/6_area do circulo.py
@@ -0,0 +1,5 @@
+raio_circulo = float(input('Digite o valor do raio do círculo em centímetros: '))
+
+area_circulo = 3.14 * (raio_circulo * raio_circulo)
+
+print(f'O valor da área do círculo é aproximadamente {area_circulo} cm².')
