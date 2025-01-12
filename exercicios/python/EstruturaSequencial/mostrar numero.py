52bf98b5445481c07f0b67ebb790e1cbe533eeae
Letícia <151076624+leticiamnteiro@users.noreply.github.com>
2025-01-12 12:02:17 -0300

    Add files via upload

---
+++ exercicios/python/EstruturaSequencial/mostrar numero.py
@@ -0,0 +1,3 @@
+numero = int(input('Digite um número: '))
+
+print(f'O número que você informado foi {numero}')
---
+++ exercicios/python/EstruturaSequencial/soma de inteiros.py
@@ -0,0 +1,4 @@
+numero_1 = int(input('Digite um número: '))
+numero_2 = int(input('Digite um número: '))
+
+print(f'A soma de {numero_1} e {numero_2} é {numero_1 + numero_2}')
